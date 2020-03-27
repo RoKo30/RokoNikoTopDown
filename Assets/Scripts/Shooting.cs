801e334c2651bd8e591548b532cb89ec47ce2fc5
RoKo30 <62470983+RoKo30@users.noreply.github.com>
2020-03-27 20:48:39 +0100

    First commit

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+  public GameObject Bullet;
+  public Rigidbody BulletRb;
+  public Transform GunTransform;
+    Vector3 vector;
+
+  void Update()
+  {
+   if (Input.GetMouseButtonDown(0))
+   {
+     Instantiate(Bullet, transform.position, transform.rotation);
+   }
+  }
+  void FixedUpdate()
+  {
+     BulletRb.AddForce(0,0,6000*Time.deltaTime);
+  }
+}
